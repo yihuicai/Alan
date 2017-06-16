d975b80757fdb59eedf8ea883a31ff7932867849
yihuicai <yihuicai@buffalo.edu>
2017-06-16 15:41:16 -0700

    added demo section in all_catagory

--- application.py
+++ application.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import session as login_session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Catagory, Item
+import random
+import string
 
 app = Flask(__name__)
 engine=create_engine('sqlite:///catalog.db')
@@ -9,12 +12,26 @@ Base.metadata.bind=engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+@app.route('/catalog/<int:catalog_id>/item/<int:item_id>')
+def Items(catalog_id, item_id):
+    item=session.query(Item).filter_by(Id=item_id).one()
+    catalog=session.query(Catagory).filter_by(Id=catalog_id).one()
+    return render_template('Item.html', item=item, catalog=catalog)
+
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    return "The current session state is %s" % login_session['state']
+
 @app.route('/')
 @app.route('/catalog')
 def All_catalog():
-    
     catalog= session.query(Catagory).all()
     catagory=[]
+    latest=session.query(Item).order_by(Item.Id).limit(2)
     for i in catalog:
         cata_item={}
         cata_item['id']=i.Id
@@ -22,7 +39,7 @@ def All_catalog():
         items= session.query(Item).filter_by(catagory_id=i.Id).all()
         cata_item['items']=items
         catagory.append(cata_item)
-    return render_template('all_catalog.html',catalog=catagory)
+    return render_template('all_catalog.html',catalog=catagory,latest=latest)
     
 @app.route('/catalog/<int:catalog_id>')
 @app.route('/catalog/<int:catalog_id>/item')
@@ -35,7 +52,7 @@ def This_catalog(catalog_id):
     items=session.query(Item).filter_by(catagory_id=catalog.Id).all()
     cata_item['items']=items
     catagory.append(cata_item)
-    return render_template('all_catalog.html',catalog=catagory)
+    return render_template('all_catalog.html',catalog=catagory,latest=[])
     
 @app.route('/catalog/new', methods=['GET','POST'])
 def New_catalog():
